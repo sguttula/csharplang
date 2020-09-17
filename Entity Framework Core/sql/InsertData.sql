44bdef5fd4d1264a53b1ca6366b2f7d38ae9b07d
Sudarshan Guttula <sidguttula@yahoo.com>
2020-09-16 17:41:10 -0700

    This script populates the tables with some test data

---
+++ Entity Framework Core/sql/InsertData.sql
@@ -0,0 +1,12 @@
+﻿INSERT INTO [Forums] (Title) VALUES (N'CS 101');
+
+GO
+
+INSERT INTO [Topics] (Title, Description, ForumId) VALUES (N'Homework 1', N'Ask all your questions about homework 1 here.', 1);
+
+GO
+
+INSERT INTO [Replies] (Message, TopicId) VALUES (N'Has anyone finished yet?', 1);
+INSERT INTO [Replies] (Message, TopicId) VALUES (N'Nope.', 1);
+
+GO

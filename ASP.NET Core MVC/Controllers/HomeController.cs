8f523137bd6d5672e2b004dad119a88b705e16b6
Sudarshan Guttula <sidguttula@yahoo.com>
2020-09-18 13:37:29 -0700

    Home controller IActionResult route

---
+++ ASP.NET Core MVC/Controllers/HomeController.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using HW3.Models;
+
+namespace HW3.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult List()
+        {
+            return View();
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+        public IActionResult Add()
+        {
+            return View();
+        }
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}

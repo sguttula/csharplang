f494d4811c5fa9b188077ea9e97b552abc718ef9
Sudarshan Guttula <sidguttula@yahoo.com>
2020-09-18 13:39:37 -0700

    Topic controller IActionResult route

---
+++ ASP.NET Core MVC/Controllers/TopicController.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Mvc;
+using HW3.Models;
+using static HW3.Services.TopicService;
+
+namespace HW3.Controllers
+{
+    public class TopicController : Controller
+    {
+        private readonly ITopicService topicService;
+        public TopicController(ITopicService topicService)
+        {
+            this.topicService = topicService;
+        }
+        /*
+        public IActionResult Index()
+        {
+            return View();
+        }
+        */
+        public IActionResult List()
+        {
+            var topic = topicService.GetTopics();
+            return View(topic);
+        }
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+        [HttpGet]
+        public IActionResult Add()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Add(Topics topic)
+        {
+            topic.Id = topic.Id;
+            topic.TopicName = topic.TopicName;
+            topic.TopicTimeStamp = topic.TopicTimeStamp;
+            topicService.AddTopic(topic);
+            return RedirectToAction(nameof(List));
+        }
+    }
+}

3cbb6a215791d2f37886637996b55cca2eae34d7
Sudarshan Guttula <sidguttula@yahoo.com>
2020-09-22 10:59:45 -0700

    Model class

---
+++ EF Core .NET/Person.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Lab4
+{
+    public class Person
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int Age { get; set; }
+        //public virtual Employee Leader { get; set; }
+
+        public override string ToString()
+        {
+            return $"[{Id}, {Name}, {Age}]";
+        }
+    }
+}

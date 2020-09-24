c5399864dedad7f6e29252279fdc96681e43c009
Sudarshan Guttula <sidguttula@yahoo.com>
2020-09-24 12:40:42 -0700

    Home controller service

---
+++ MVC Web App/Controllers/HomeController.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Lab5.Models;
+
+namespace Lab5.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult List()
+        {
+            return View();
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+        public IActionResult Add()
+        {
+            return View();
+        }
+        /*
+        public IActionResult ContactInfo()
+        {
+            return View();
+        }
+        */
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
